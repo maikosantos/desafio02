c8f8dacdc736a05f296f4cd4cb1127aa9ed14a30
Maiko <maikossmaster@gmail.com>
2018-05-29 13:47:48 -0300

    ...

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,7 +4,7 @@ import { Container, Div } from './styles';
 import Header from '../../components/header';
 import SideBar from '../../components/sideBar';
 import ContainerIssues from '../../components/listIssues';
-
+// teste
 class Main extends Component {
   render() {
     return (
