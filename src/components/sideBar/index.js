39f79f6fc9bdaac2a7b12bf162e377794fbf980c
maiko_silva <maikossmaster@gmail.com>
2018-05-28 22:05:23 -0300

    ...

--- src/components/sideBar/index.js
+++ src/components/sideBar/index.js
@@ -4,7 +4,7 @@ import { Container } from './styles';
 
 const SideBar = () => (
   <Container>
-    <div>Opa, beleza?</div>
+    <div>Opa, beleza??</div>
     <div>Opa, beleza?</div>
     <div>Opa, beleza?</div>
     <div>Opa, beleza?</div>
